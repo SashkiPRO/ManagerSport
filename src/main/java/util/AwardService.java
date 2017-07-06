7ef99ecedf218cbbb343b559dd008e485d9f00ef
SashkiPRO <oleksandr.botsula@nure.ua>
2017-07-06 14:00:37 +0300

    Add new interface

---
+++ src/main/java/util/AwardService.java
@@ -0,0 +1,14 @@
+package util;
+
+import net.proselyte.springsecurityapp.model.Award;
+
+import java.util.List;
+
+/**
+ * Created by Alexandr on 14.06.2017.
+ */
+public interface AwardService {
+    void save(Award award);
+    List<Award> findAll();
+    void delete(Award award);
+}
