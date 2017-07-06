bfb51d3998f563d47da39a8dff3f7f08d2d64eca
SashkiPRO <oleksandr.botsula@nure.ua>
2017-07-06 14:15:18 +0300

    Change date

--- src/main/java/ua/nure/botsula/service/AwardService.java
+++ src/main/java/ua/nure/botsula/service/AwardService.java
@@ -5,10 +5,11 @@ import net.proselyte.springsecurityapp.model.Award;
 import java.util.List;
 
 /**
- * Created by Alexandr on 14.06.2017.
+ * Created by Alexandr on 15.06.2017.
  */
 public interface AwardService {
     void save(Award award);
     List<Award> findAll();
     void delete(Award award);
+
 }
